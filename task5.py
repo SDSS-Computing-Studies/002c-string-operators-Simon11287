f0dcd93d567985b270f0c329b6cd63619cc58af8
Simon11287 <71406841+Simon11287@users.noreply.github.com>
2020-11-13 12:16:59 -0800

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,17 +1,8 @@
 #!python3
-
-start = "here" repeats = 4
-word = "kitty "
-
-start + repeats*word = result
-
-  print(result)
-
-  start = "here"
-word = "kitty "
+start = "here"
+word = " kitty "
 x = 3
 
-start + " "(word * x)= result
+print(start +(word*3))
 
-print(result)
   
